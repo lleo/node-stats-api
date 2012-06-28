e6ab03cab828215837bd99b6efb5a1c6dc29553c
LLeo <lleoem@gmail.com>
2012-06-27 23:29:03 -0400

    made it work

--- test/t-zero-value-bytes.js
+++ test/t-zero-value-bytes.js
@@ -20,12 +20,12 @@ function log2(v) { return Math.log(v)/Math.LN2 }
 function order2(v) { return Math.floor(log2(v)) }
 
 var v = 0
-log("Math.log(%d) => %d", v, Math.log(v))
-log("log2(%d) => %d", v, log2(v))
+//log("Math.log(%d) => %d", v, Math.log(v))
+//log("log2(%d) => %d", v, log2(v))
 
-//stats.createStat('file_sz', 'Value')
-////stats.createHog('file_sz_hog', 'file_sz', 'Bytes')
-//stats.createHog('file_sz_hog', 'file_sz', 'LogBytes')
-//
-//stats.get('file_sz').set(0)
-//log(stats.toString())
+stats.createStat('file_sz', statsmod.Value)
+stats.createHog('file_sz_hog', 'file_sz', statsmod.Bytes)
+stats.createHog('file_sz_hog', 'file_sz', statsmod.LogBytes)
+
+stats.get('file_sz').set(0)
+log(stats.toString())
